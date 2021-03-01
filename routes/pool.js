84895acfcc30a073638134a680efce515f065aff
naman0058 <jnaman345@gmail.com>
2021-03-01 17:02:11 +0530

    a

---
+++ routes/pool.js
@@ -0,0 +1,17 @@
+var mysql = require('mysql')
+require('dotenv').config()
+
+const pool = mysql.createPool({
+
+   host:'localhost',
+  ///host : 'localhost',
+   user: 'root',
+  password:'123',
+  // password : '123',
+    database: 'e-commerce',
+    port:'3306' ,
+    multipleStatements: true
+  })
+
+
+module.exports = pool;
